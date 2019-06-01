e6e4707802584a8857a48f886878bcec03d132fd
andreafavia <s237758@studenti.polito.it>
2019-06-01 12:18:20 +0200

    main

--- categoriesTasks.js
+++ categoriesTasks.js
@@ -1,11 +1,12 @@
 
 /*
 @author: Andrea Favia
+@id : 237758
  */
 const mean = arr =>arr.reduce( (p,c) => +p + (+c),0)/arr.length;
 
-function groupByCategory(data){
-    groups = _.groupBy(data,'Category');
+var groupByCategory = (data)=>{
+    let groups = _.groupBy(data,'Category');
     let categories = Object.keys(groups); //get categories
     //Number((6.688689).toFixed(1)); // 6.7
     let vect = [];
@@ -29,10 +30,13 @@ function groupByCategory(data){
         vect.push(obj);
     }
     return vect;
+};
 
+
+function plotStatsCategories(data) {
+    plotRatings(data);
+    plotReviews(data);
 }
-
-
 function plotRatings(data){
     sorted = _.sortBy(data, d=>d.rating_mean);
     trace = [{
@@ -61,51 +65,4 @@ function plotReviews(data){
 
     Plotly.newPlot('reviews_mean',trace)
 
-}
-
-
-
-
-function tableToHtmlElement(data) {
-    let res = document.createElement("table");
-    let html = "<tr>";
-    for (let h in data[0])
-        if (data[0].hasOwnProperty(h))
-            html += "<th>" + h + "</th>";
-    html += "</tr>";
-    for (let i = 0; i < data.length; ++i) {
-        html += "<tr>";
-        for (let f in data[i])
-            if (data[i].hasOwnProperty(f))
-                html += "<td>" + data[i][f] + "</td>";
-        html += "</tr>";
-    }
-    html += "</table>";
-    res.innerHTML = html;
-    return res;
-}
-
-
-function csvParse(csv) {
-    'use strict';
-    let csvRE = /(,|^|\n|\r|\r\n)[ \t]*(?:([^",\n\r]*)|"((?:[^"]*|"")*)")[ \t]*/g;
-    let heads = [], rows = [];
-    let row, col, line = -1;
-    while (true) {
-        let match = csvRE.exec(csv);
-        if (!match) break;
-        if (match[1] !== ',') {
-            if (row) rows.push(row);
-            line++;
-            row = line && {};
-            col = 0;
-        }
-        let cell = match[2] ||
-            (match[3] || "").replace(/""/g, '"');
-        if (line === 0) heads.push(cell);
-        else row[heads[col++] || "C" + col] = cell;
-    }
-
-    if (JSON.stringify(row) !== "{}") rows.push(row);
-    return rows;
 }
--- main.js
+++ main.js
@@ -2,23 +2,45 @@ url = 'play_store_cleaned.csv';
 data = null;
 
 window.onload = function () {
-    getData(url,20);
-    // first task
-    var grouped = groupByCategory(data);
-    plotRatings(grouped);
+    getData(url,20,function () {
+
+        var groups = groupByCategory(data);
+        plotStatsCategories(groups)
 
 
+
+    });
 
 };
 
-function  getData(url,n) {
+function  getData(url,n,callback) {
     $.get(url,function (csv) {
         data = $.csv.toObjects(csv);
         // show first n values
         let table = tableToHtmlElement(data.slice(0,n));
         $("#csv").append(table);
 
+        callback()
     });
 
-
 };
+
+function tableToHtmlElement(data) {
+    let res = document.createElement("table");
+    let html = "<tr>";
+    for (let h in data[0])
+        if (data[0].hasOwnProperty(h))
+            html += "<th>" + h + "</th>";
+    html += "</tr>";
+    for (let i = 0; i < data.length; ++i) {
+        html += "<tr>";
+        for (let f in data[i])
+            if (data[i].hasOwnProperty(f))
+                html += "<td>" + data[i][f] + "</td>";
+        html += "</tr>";
+    }
+    html += "</table>";
+    res.innerHTML = html;
+    return res;
+};
+
